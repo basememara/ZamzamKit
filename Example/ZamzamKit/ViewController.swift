7559273253375e7a52d433859434ff8107dbf083
Basem Emara <basememara@gmail.com>
2015-06-09 12:44:44 -0400

    Initial commit

---
+++ Example/ZamzamKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ZamzamKit
+//
+//  Created by Basem Emara on 06/09/2015.
+//  Copyright (c) 06/09/2015 Basem Emara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
