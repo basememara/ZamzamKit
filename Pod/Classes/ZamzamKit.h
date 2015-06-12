7adbc881a70cea030b0e9c708fb308e565377abe
Basem Emara <basememara@gmail.com>
2015-06-12 17:40:13 -0400

    Testing new header.

---
+++ Pod/Classes/ZamzamKit.h
@@ -0,0 +1,19 @@
+//
+//  ZamzamKit.h
+//  ZamzamKit
+//
+//  Created by Basem Emara on 5/6/15.
+//  Copyright (c) 2015 Zamzam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZamzamKit.
+FOUNDATION_EXPORT double ZamzamKitVersionNumber;
+
+//! Project version string for ZamzamKit.
+FOUNDATION_EXPORT const unsigned char ZamzamKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZamzamKit/PublicHeader.h>
+
+
