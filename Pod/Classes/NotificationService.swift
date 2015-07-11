43cf57df6a85ef1dc0166efeacdc28115e361541
Basem Emara <basememara@gmail.com>
2015-07-11 17:29:08 -0400

    Added get by notification API.

--- Pod/Classes/NotificationService.swift
+++ Pod/Classes/NotificationService.swift
@@ -105,4 +105,19 @@ public class NotificationService: NSObject {
         return false
     }
     
+    public func getByIdentifier(application: UIApplication, _ indentifier: String) -> UILocalNotification? {
+        if let notifications = application.scheduledLocalNotifications {
+            for item in notifications {
+                // Find matching to delete
+                if let notification = item as? UILocalNotification,
+                    let userInfo = notification.userInfo as? [String: String]
+                    where userInfo["indentifier"] == indentifier {
+                        return notification
+                }
+            }
+        }
+        
+        return nil
+    }
+    
 }
