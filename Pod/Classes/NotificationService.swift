4646777a0e358e505f752ca1e340b8a96b6a6282
Basem Emara <basememara@gmail.com>
2015-07-12 08:16:58 -0400

    Added notifications create to API.

--- Pod/Classes/NotificationService.swift
+++ Pod/Classes/NotificationService.swift
@@ -35,25 +35,20 @@ public class NotificationService: NSObject {
             let notificationSettings = UIUserNotificationSettings(
                 forTypes: type,
                 categories: NSSet(objects: mainCategory) as Set<NSObject>)
-    
+            
             // Register notifications
             application.registerUserNotificationSettings(notificationSettings)
     }
     
-    public func schedule(application: UIApplication, date: NSDate, body: String,
+    public func create(date: NSDate,
+        body: String,
         title: String? = nil,
-        indentifier: String? = nil,
+        identifier: String? = nil,
         category: String = "mainCategory",
         badge: Int = 0,
         sound: String? = UILocalNotificationDefaultSoundName,
         repeat: NSCalendarUnit? = nil,
-        incrementDayIfPast: Bool = true,
-        removeDuplicates: Bool = false) {
-            // De-dup previous notifications if applicable
-            if let id = indentifier where removeDuplicates {
-                remove(application, id)
-            }
-            
+        incrementDayIfPast: Bool = true) -> UILocalNotification {
             // Initialize and configure notification
             var notification = UILocalNotification()
             notification.category = category
@@ -67,36 +62,63 @@ public class NotificationService: NSObject {
             if let r = repeat {
                 notification.repeatInterval = r
             }
-        
+            
             // Provide unique identifier for later use
-            if let id = indentifier {
-                notification.userInfo = ["indentifier": id]
+            if let id = identifier {
+                notification.userInfo = ["identifier": id]
             }
+            
+            return notification
+    }
+    
+    public func schedule(application: UIApplication, date: NSDate, body: String,
+        title: String? = nil,
+        identifier: String? = nil,
+        category: String = "mainCategory",
+        badge: Int = 0,
+        sound: String? = UILocalNotificationDefaultSoundName,
+        repeat: NSCalendarUnit? = nil,
+        incrementDayIfPast: Bool = true,
+        removeDuplicates: Bool = false) {
+            // De-dup previous notifications if applicable
+            if let id = identifier where removeDuplicates {
+                remove(application, id)
+            }
+            
+            var notification = create(date,
+                body: body,
+                title: title,
+                identifier: identifier,
+                category: category,
+                badge: badge,
+                sound: sound,
+                repeat: repeat,
+                incrementDayIfPast: incrementDayIfPast)
             
             application.scheduleLocalNotification(notification)
     }
     
-    public func remove(application: UIApplication, _ indentifier: String) {
+    public func remove(application: UIApplication, _ identifier: String) {
         if let notifications = application.scheduledLocalNotifications {
             for item in notifications {
                 // Find matching to delete
                 if let notification = item as? UILocalNotification,
                     let userInfo = notification.userInfo as? [String: String]
-                        where userInfo["indentifier"] == indentifier {
-                    // Cancel notification
-                    application.cancelLocalNotification(notification)
+                    where userInfo["identifier"] == identifier {
+                        // Cancel notification
+                        application.cancelLocalNotification(notification)
                 }
             }
         }
     }
     
-    public func exists(application: UIApplication, _ indentifier: String) -> Bool {
+    public func exists(application: UIApplication, _ identifier: String) -> Bool {
         if let notifications = application.scheduledLocalNotifications {
             for item in notifications {
                 // Find matching to delete
                 if let notification = item as? UILocalNotification,
                     let userInfo = notification.userInfo as? [String: String]
-                    where userInfo["indentifier"] == indentifier {
+                    where userInfo["identifier"] == identifier {
                         return true
                 }
             }
@@ -105,7 +127,7 @@ public class NotificationService: NSObject {
         return false
     }
     
-    public func getByIdentifier(application: UIApplication, _ indentifier: String) -> [UILocalNotification] {
+    public func getByIdentifier(application: UIApplication, _ identifier: String) -> [UILocalNotification] {
         var matchedNotifications: [UILocalNotification] = []
         
         if let notifications = application.scheduledLocalNotifications {
@@ -113,7 +135,7 @@ public class NotificationService: NSObject {
                 // Find matching to delete
                 if let notification = item as? UILocalNotification,
                     let userInfo = notification.userInfo as? [String: String]
-                    where userInfo["indentifier"] == indentifier {
+                    where userInfo["identifier"] == identifier {
                         matchedNotifications.append(notification)
                 }
             }
