c492030c0410154524d59ecdba25f2339df75181
Basem Emara <basememara@gmail.com>
2015-08-05 09:42:59 -0400

    Added query string functions.

--- Pod/Classes/WebService.swift
+++ Pod/Classes/WebService.swift
@@ -25,4 +25,55 @@ public class WebService: NSObject {
         return attributedString.string
     }
     
+    /**
+    Add, update, or remove a query string item from the URL
+    
+    :param: url   the URL
+    :param: key   the key of the query string item
+    :param: value the value to replace the query string item, nil will remove item
+    
+    :returns: the URL with the mutated query string
+    */
+    public func addOrUpdateQueryString(url: String, key: String, value: String?) -> String {
+        if let components = NSURLComponents(string: url),
+            var queryItems = (components.queryItems ?? []) as? [NSURLQueryItem] {
+                println("sdvs")
+                for (index, item) in enumerate(queryItems) {
+                    // Match query string key and update
+                    if item.name == key {
+                        if let v = value {
+                            queryItems[index] = NSURLQueryItem(name: key, value: v)
+                        } else {
+                            queryItems.removeAtIndex(index)
+                        }
+                        components.queryItems = queryItems.count > 0
+                            ? queryItems : nil
+                        return components.string!
+                    }
+                }
+                
+                // Key doesn't exist if reaches here
+                if let v = value {
+                    // Add key to URL query string
+                    queryItems.append(NSURLQueryItem(name: key, value: v))
+                    components.queryItems = queryItems
+                    return components.string!
+                }
+        }
+        
+        return url
+    }
+    
+    /**
+    Removes a query string item from the URL
+    
+    :param: url   the URL
+    :param: key   the key of the query string item
+    
+    :returns: the URL with the mutated query string
+    */
+    public func removeQueryString(url: String, key: String) -> String {
+        return addOrUpdateQueryString(url, key: key, value: nil)
+    }
+    
 }
