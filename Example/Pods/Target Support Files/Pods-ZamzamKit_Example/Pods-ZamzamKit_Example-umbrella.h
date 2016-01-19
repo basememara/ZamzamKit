3af5f5f4aa055f0c8fcedbda7dfa80c5d1e19665
Basem Emara <basememara@gmail.com>
2016-01-19 18:03:33 -0500

    Update Xcode and CocoaPod settings.

---
+++ Example/Pods/Target Support Files/Pods-ZamzamKit_Example/Pods-ZamzamKit_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ZamzamKit_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ZamzamKit_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-ZamzamKit_Tests/Pods-ZamzamKit_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ZamzamKit_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ZamzamKit_TestsVersionString[];
+
