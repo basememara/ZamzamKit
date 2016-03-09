30083f3e4f343e1bcdb6d5003bbd4d64a4e75978
Basem Emara <basememara@gmail.com>
2016-03-09 07:35:12 -0500

    Added location protocol.

---
+++ Sources/Protocols/ViewControllerLocatable.swift
@@ -0,0 +1,40 @@
+//
+//  ViewControllerLocatable.swift
+//  ZamzamKit
+//
+//  Created by Basem Emara on 3/9/16.
+//  Copyright © 2016 Zamzam. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+protocol ViewControllerLocatable: CLLocationManagerDelegate {
+    var locationManager: CLLocationManager? { get set }
+    
+    func updateLocation(locations: [CLLocation]?)
+}
+
+extension ViewControllerLocatable {
+    
+    func setupLocationManager(
+        desiredAccuracy: CLLocationAccuracy = kCLLocationAccuracyThreeKilometers,
+        distanceFilter: Double = 1000.0) {
+            // Start requesting GPS location
+            if locationManager == nil {
+                locationManager = CLLocationManager(self)
+            } else {
+                locationManager?.tryStartUpdating()
+            }
+    }
+    
+    func updateLocation(location: CLLocation?) {
+        guard let location = location else { return }
+        updateLocation([location])
+    }
+    
+    func updateLocation(latitude latitude: Double, longitude: Double) {
+        updateLocation([CLLocation(latitude: latitude, longitude: latitude)])
+    }
+    
+}
