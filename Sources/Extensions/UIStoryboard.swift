c4893b48135aa52d3911ae173dfda5b4e695e8f1
Basem Emara <basememara@gmail.com>
2016-05-26 12:17:54 -0400

    Added UIStoryboard extension for convenience initializer without explicit bundle parameter and a function for instantiating view controller via convention.

---
+++ Sources/Extensions/UIStoryboard.swift
@@ -0,0 +1,32 @@
+//
+//  UIStoryboard.swift
+//  ZamzamKit
+//
+//  Created by Basem Emara on 5/26/16.
+//  Copyright © 2016 Zamzam. All rights reserved.
+//
+
+import UIKit
+
+public extension UIStoryboard {
+
+    /**
+     Creates and returns a storyboard object for the specified storyboard resource file in the main bundle of the current application.
+
+     - parameter name: The name of the storyboard resource file without the filename extension.
+
+     - returns: A storyboard object for the specified file. If no storyboard resource file matching name exists, an exception is thrown.
+     */
+    public convenience init(name: String) {
+        self.init(name: name, bundle: nil)
+    }
+    
+    /**
+     Instantiate view controller using convention of storyboard identifier matching class name.
+
+     - returns: Instantiated controller from storyboard, or nil if non existent.
+     */
+    public func instantiateViewController<T: UIViewController>() -> T? {
+        return instantiateViewControllerWithIdentifier(String(T.self)) as? T
+    }
+}
