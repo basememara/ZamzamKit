b6ca18586beb5d6d39139e24a9e680c975895922
Basem Emara <contact@basememara.com>
2016-11-01 08:16:01 -0400

    Exposed jsonString API to be public.

--- Sources/Extensions/Collection.swift
+++ Sources/Extensions/Collection.swift
@@ -18,7 +18,7 @@ public extension Collection {
 extension Collection where Iterator.Element == (String, Any) {
     
     /// Converts collection of objects to JSON string
-    var jsonString: String? {
+    public var jsonString: String? {
         guard let data = self as? NSDictionary,
             let stringData = try? JSONSerialization.data(withJSONObject: data, options: [])
             else { return nil }
--- Sources/Extensions/Sequence.swift
+++ Sources/Extensions/Sequence.swift
@@ -11,7 +11,7 @@ import Foundation
 public extension Sequence {
 
     /// Converts collection of objects to JSON string
-    var jsonString: String? {
+    public var jsonString: String? {
         guard let data = self as? [[String: Any]],
             let stringData = try? JSONSerialization.data(withJSONObject: data, options: [])
                 else { return nil }
