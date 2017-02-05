541b55841373c31ce719a623096a5a3ba9e7e0ae
Basem Emara <contact@basememara.com>
2017-02-05 01:30:11 -0500

    Added Thread extension with convenient async/sync functions to mimic GCD API.

---
+++ Sources/Extensions/Thread.swift
@@ -0,0 +1,41 @@
+//
+//  Thread.swift
+//  ZamzamKit
+//
+//  Created by Basem Emara on 2/5/17.
+//  Copyright © 2017 Zamzam. All rights reserved.
+//
+
+import Foundation
+
+extension Thread {
+
+    private typealias Block = @convention(block) () -> Void
+    
+    /**
+     Execute block, used internally for async/sync functions.
+     
+     - parameter block: Process to be executed.
+     */
+    @objc private func run(block: Block) {
+        block()
+    }
+
+    /**
+     Perform block on current thread asynchronously.
+     
+     - parameter block: Process to be executed.
+     */
+    public func async(execute: Block) {
+        perform(#selector(run(block:)), on: self, with: execute, waitUntilDone: false)
+    }
+
+    /**
+     Perform block on current thread synchronously.
+     
+     - parameter block: Process to be executed.
+     */
+    public func sync(execute: Block) {
+        perform(#selector(run(block:)), on: self, with: execute, waitUntilDone: true)
+    }
+}
