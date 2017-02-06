509fb505e49524efbaea76b62de33dc2b8233d60
Basem Emara <contact@basememara.com>
2017-02-06 02:11:45 -0500

    Added error enum for error handling.

---
+++ Sources/Enums/ZamzamError.swift
@@ -0,0 +1,13 @@
+//
+//  ZamzamError.swift
+//  ZamzamKit
+//
+//  Created by Basem Emara on 2/6/17.
+//  Copyright © 2017 Zamzam. All rights reserved.
+//
+
+import Foundation
+
+enum ZamzamError: Error {
+    case invalidData
+}
