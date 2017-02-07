76067c604ee28e6206493e9a0bdf776f957c1a6a
Basem Emara <contact@basememara.com>
2017-02-07 10:51:13 -0500

    Added user notification attachment extension to download link from the web.

---
+++ Sources/Extensions/UNNotificationAttachment.swift
@@ -0,0 +1,30 @@
+//
+//  UNNotificationAttachment.swift
+//  ZamzamKit
+//
+//  Created by Basem Emara on 2/7/17.
+//  Copyright © 2017 Zamzam. All rights reserved.
+//
+
+import UserNotifications
+
+@available(iOS 10.0, *)
+public extension UNNotificationAttachment {
+
+    /// Converts a resource from the Internet to a user notification attachment.
+    ///
+    /// - Parameters:
+    ///   - link: The remote HTTP path to the resource.
+    ///   - identifier: The identitifer of the user notification attachment.
+    ///   - complete: The callback with the constucted user notification attachment.
+    static func download(from link: String, identifier: String? = nil, complete: @escaping (Result<UNNotificationAttachment>) -> Void) {
+        FileManager.default.download(from: link) {
+            guard $0.2 == nil else { return complete(.failure($0.2 ?? ZamzamError.general)) }
+            
+            guard let url = $0.0, let attachment = try? UNNotificationAttachment(identifier: identifier ?? link, url: url)
+                else { return complete(.failure(ZamzamError.invalidData)) }
+            
+            complete(.success(attachment))
+        }
+    }
+}
