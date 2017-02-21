765353d414289a67a3a1acc1b5e2396509caa5e4
Basem Emara <contact@basememara.com>
2017-02-21 00:07:37 -0500

    Add UINavigationController extension for transparent navigation bar.

---
+++ Sources/Extensions/UINavigationBar.swift
@@ -0,0 +1,41 @@
+//
+//  UINavigationBar.swift
+//  ZamzamKit
+//
+//  Created by Basem Emara on 2/21/17.
+//  Copyright © 2017 Zamzam. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationBar {
+
+    /// Set transparent navigation bar
+    // http://stackoverflow.com/questions/2315862/make-uinavigationbar-transparent
+    var transparent: Bool {
+        get {
+            return backgroundColor == .clear
+        }
+        
+        set {
+            if newValue {
+                // Override point for customization after application launch.
+                // Sets background to a blank/empty image
+                setBackgroundImage(UIImage(), for: .default)
+                
+                // Sets shadow (line below the bar) to a blank image
+                shadowImage = UIImage()
+                
+                // Sets the translucent background color
+                backgroundColor = .clear
+                
+                // Set translucent. (Default value is already true, so this can be removed if desired.)
+                isTranslucent = true
+            } else {
+                setBackgroundImage(nil, for: .default)
+                shadowImage = nil
+                backgroundColor = nil
+            }
+        }
+    }
+}
