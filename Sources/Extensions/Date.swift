724584cd8c0e03ef405d94c5ab3a25eb62e01acb
Basem Emara <contact@basememara.com>
2017-04-22 12:15:37 -0400

    Add normalized Gregorian calendar with UTC timezone and POSIX locale

--- Sources/Extensions/Date.swift
+++ Sources/Extensions/Date.swift
@@ -185,6 +185,17 @@ public extension Date {
     }
 }
 
+public extension Calendar {
+    
+    /// Normalize date calculations using gregorian calendar with UTC timezone and POSIX.
+    static let gregorianUTC: Calendar = {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = TimeZone(identifier: "UTC")!
+        calendar.locale = .posix
+        return calendar
+    }()
+}
+
 public extension Calendar.Component {
     static var date: Set<Calendar.Component> = { return [.year, .month, .day, .hour, .minute, .second] }()
 }
