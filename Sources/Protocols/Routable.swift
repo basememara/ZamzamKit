a7719a4fa7e0bb8fa2724a95761bef7542d2b268
Basem Emara <contact@basememara.com>
2017-05-05 07:26:57 -0400

    Use automatic context for routable extension

--- Sources/Protocols/Routable.swift
+++ Sources/Protocols/Routable.swift
@@ -11,54 +11,51 @@ import UIKit
 public protocol Routable {
     associatedtype StoryboardIdentifier: RawRepresentable
     
-    func present(storyboard: StoryboardIdentifier, identifier: String?, animated: Bool, configure: ((UIViewController) -> Void)?)
-    func push(storyboard: StoryboardIdentifier, identifier: String?, animated: Bool, configure: ((UIViewController) -> Void)?)
+    func show<T: UIViewController>(storyboard: StoryboardIdentifier, identifier: String?, configure: ((T) -> Void)?)
+    func showDetailViewController<T: UIViewController>(storyboard: StoryboardIdentifier, identifier: String?, configure: ((T) -> Void)?)
 }
 
 public extension Routable where Self: UIViewController, StoryboardIdentifier.RawValue == String {
     
     /**
-     Present the intial view controller of the specified storyboard.
-     Be sure to set the initial view controller in the target storyboard.
+     Present the intial view controller of the specified storyboard in the primary context.
+     Set the initial view controller in the target storyboard or specify the identifier.
 
      - parameter storyboard: Storyboard name.
      - parameter identifier: View controller name.
-     - parameter animated: Animates the presentation of the storyboard.
      - parameter configure: Configure the view controller before it is loaded.
      */
-    func present(storyboard: StoryboardIdentifier, identifier: String? = nil, animated: Bool = true, configure: ((UIViewController) -> Void)? = nil) {
+    func show<T: UIViewController>(storyboard: StoryboardIdentifier, identifier: String? = nil, configure: ((T) -> Void)? = nil) {
         let storyboard = UIStoryboard(name: storyboard.rawValue)
         
         guard let controller = (identifier != nil
             ? storyboard.instantiateViewController(withIdentifier: identifier!)
-            : storyboard.instantiateInitialViewController())
-            else { return assertionFailure("Invalid controller for storyboard \(String(describing: storyboard)).") }
+            : storyboard.instantiateInitialViewController()) as? T
+            else { return assertionFailure("Invalid controller for storyboard \(storyboard).") }
         
         configure?(controller)
         
-        present(controller, animated: animated, completion: nil)
+        show(controller, sender: self)
     }
     
     /**
-     Pushes the intial view controller of the specified storyboard to the navigation stack.
-     Be sure to set the initial view controller in the target storyboard.
+     Present the intial view controller of the specified storyboard in the secondary (or detail) context.
+     Set the initial view controller in the target storyboard or specify the identifier.
 
      - parameter storyboard: Storyboard name.
      - parameter identifier: View controller name.
-     - parameter animated: Animates the presentation of the storyboard.
      - parameter configure: Configure the view controller before it is loaded.
      */
-    func push(storyboard: StoryboardIdentifier, identifier: String? = nil, animated: Bool = true, configure: ((UIViewController) -> Void)? = nil) {
+    func showDetailViewController<T: UIViewController>(storyboard: StoryboardIdentifier, identifier: String? = nil, configure: ((T) -> Void)? = nil) {
         let storyboard = UIStoryboard(name: storyboard.rawValue)
         
-        guard let navigationController = navigationController,
-            let controller = (identifier != nil
-                ? storyboard.instantiateViewController(withIdentifier: identifier!)
-                : storyboard.instantiateInitialViewController())
-            else { return assertionFailure("Invalid controller for storyboard \(String(describing: storyboard)).") }
+        guard let controller = (identifier != nil
+            ? storyboard.instantiateViewController(withIdentifier: identifier!)
+            : storyboard.instantiateInitialViewController()) as? T
+            else { return assertionFailure("Invalid controller for storyboard \(storyboard).") }
         
         configure?(controller)
-                
-        navigationController.pushViewController(controller, animated: animated)
+        
+        showDetailViewController(controller, sender: self)
     }
 }
