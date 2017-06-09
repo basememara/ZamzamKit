9881ea45d6810841a180c71816826eeb73a718e3
Basem Emara <contact@basememara.com>
2017-06-09 16:39:30 -0400

    Add extend complications extension for watch complication server object

--- Sources/Extensions/CLKComplicationServer.swift
+++ Sources/Extensions/CLKComplicationServer.swift
@@ -12,11 +12,19 @@ import ClockKit
 public extension CLKComplicationServer {
     
     /**
-     Invalidates and reloads all timeline data
+     Invalidates and reloads all timeline data for all complications.
      */
     func reloadTimelineForComplications() {
         guard let complications = activeComplications, !complications.isEmpty else { return }
         complications.forEach { reloadTimeline(for: $0) }
+    }
+    
+    /**
+     Extends all timeline data for all complications.
+     */
+    func extendTimelineForComplications() {
+        guard let complications = activeComplications, !complications.isEmpty else { return }
+        complications.forEach { extendTimeline(for: $0) }
     }
     
 }
