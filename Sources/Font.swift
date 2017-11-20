78d7ad0d663d51f8d556fa6f66d0efe7d418d158
Basem Emara <contact@basememara.com>
2017-11-20 11:34:49 -0500

    Add font extension

---
+++ Sources/Font.swift
@@ -0,0 +1,18 @@
+//
+//  Font.swift
+//  ZamzamKit
+//
+//  Created by Basem Emara on 2017-11-20.
+//  Copyright © 2017 Zamzam. All rights reserved.
+//
+
+import UIKit
+
+public extension UIFont {
+    
+    /// Specify font trait while leaving size intact.
+    func with(traits: UIFontDescriptorSymbolicTraits...) -> UIFont {
+        let descriptor = fontDescriptor.withSymbolicTraits(UIFontDescriptorSymbolicTraits(traits))
+        return UIFont(descriptor: descriptor!, size: 0)
+    }
+}
