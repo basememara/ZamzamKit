e5df3e5bb6b54c39913449c955e54201571d9005
Basem Emara <contact@basememara.com>
2018-01-09 14:16:31 -0500

    Add UIStackView extension to add list of UIViews to arranged subviews

--- Sources/UIStackView.swift
+++ Sources/UIStackView.swift
@@ -10,6 +10,13 @@ import UIKit
 
 public extension UIStackView {
     
+    /// Adds a views to the end of the arrangedSubviews array.
+    ///
+    /// - Parameter views: List of views.
+    func addArrangedSubviews(_ views: [UIView]) {
+        views.forEach { addArrangedSubview($0) }
+    }
+    
     /// Removes and releases all arranged subviews from `UIStackView` and `superview`.
     func deleteArrangedSubviews() {
         arrangedSubviews.forEach {
