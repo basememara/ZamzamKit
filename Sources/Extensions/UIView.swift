2a209328bce339edfd237ab7157f56f6ea293a07
Basem Emara <contact@basememara.com>
2018-01-14 16:24:14 -0500

    Add extension helpers to UIView

--- Sources/Extensions/UIView.swift
+++ Sources/Extensions/UIView.swift
@@ -10,25 +10,31 @@ import Foundation
 
 public extension UIView {
 
-  var height: CGFloat {
-    get { return frame.height }
-    set { frame.size.height = newValue }
-  }
+    var height: CGFloat {
+        get { return frame.height }
+        set { frame.size.height = newValue }
+    }
 
-  var width: CGFloat {
-    get { return frame.width }
-    set { frame.size.width = newValue }
-  }
+    var width: CGFloat {
+        get { return frame.width }
+        set { frame.size.width = newValue }
+    }
 
-  var x: CGFloat {
-    get { return frame.minX }
-    set { frame.origin.x = newValue }
-  }
+    var x: CGFloat {
+        get { return frame.minX }
+        set { frame.origin.x = newValue }
+    }
 
-  var y: CGFloat {
-    get { return frame.minY }
-    set { frame.origin.y = newValue }
-  }
+    var y: CGFloat {
+        get { return frame.minY }
+        set { frame.origin.y = newValue }
+    }
+
+    /// A Boolean value that determines whether the view is visible.
+    var isVisible: Bool {
+        get { return !isHidden }
+        set { isHidden = !newValue }
+    }
 }
 
 public extension UIView {
