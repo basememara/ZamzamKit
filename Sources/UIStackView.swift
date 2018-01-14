4dbc4346fe700cd80e4590e2d5da12b43e9c7c4f
Basem Emara <contact@basememara.com>
2018-01-14 16:23:59 -0500

    Add animation extension helps to UIStackView

--- Sources/UIStackView.swift
+++ Sources/UIStackView.swift
@@ -18,10 +18,52 @@ public extension UIStackView {
     }
     
     /// Removes and releases all arranged subviews from `UIStackView` and `superview`.
-    func deleteArrangedSubviews() {
+    @discardableResult
+    func deleteArrangedSubviews() -> Self {
         arrangedSubviews.forEach {
             removeArrangedSubview($0)
             $0.removeFromSuperview()
+        }
+        
+        return self
+    }
+}
+
+public extension UIStackView {
+    
+    /// Adds a view to the end of the arrangedSubviews array with animation.
+    func addArrangedSubview(_ view: UIView, animated: Bool) {
+        guard animated else { return addArrangedSubview(view) }
+        
+        view.isHidden = true
+        view.alpha = 0
+        
+        addArrangedSubview(view)
+        
+        UIView.animate(withDuration: 0.35) {
+            view.isHidden = false
+            view.alpha = 1
+        }
+    }
+    
+    /// Adds a views to the end of the arrangedSubviews array with animation.
+    ///
+    /// - Parameter views: List of views.
+    func addArrangedSubviews(_ views: [UIView], animated: Bool) {
+        guard animated else { return addArrangedSubviews(views) }
+        
+        views.forEach {
+            $0.isHidden = true
+            $0.alpha = 0
+        }
+        
+        addArrangedSubviews(views)
+        
+        UIView.animate(withDuration: 0.35) {
+            views.forEach {
+                $0.isHidden = false
+                $0.alpha = 1
+            }
         }
     }
 }
