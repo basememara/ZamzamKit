5b8904c43db2262f1e3e1886d1ad7f26f76f1bfa
Basem Emara <contact@basememara.com>
2018-02-02 17:48:14 -0500

    Add remove observer counterpart for NotificationObservable protocol

--- Sources/Protocols/NotificationObservable.swift
+++ Sources/Protocols/NotificationObservable.swift
@@ -26,4 +26,14 @@ public extension NotificationObservable {
             object: object
         )
     }
+    
+    /// Removes matching entries from the notification center's dispatch table.
+    ///
+    /// - Parameters:
+    ///   - name: The name of the notification for which to register the observer; that is, only notifications with this name are delivered to the observer.
+    ///   - observer: Object registering as an observer. This value will use `self` if nil.
+    ///   - object: The object whose notifications the observer wants to receive; that is, only notifications sent by this sender are delivered to the observer.
+    func removeObserver(for name: NSNotification.Name, from observer: Any? = nil, object: Any? = nil) {
+        NotificationCenter.default.removeObserver(observer ?? self, name: name, object: object)
+    }
 }
