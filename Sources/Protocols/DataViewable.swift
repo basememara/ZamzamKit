bf425d2f0eefddbf385a70c633de337cb27e9583
Basem Emara <contact@basememara.com>
2018-02-04 12:20:05 -0500

    Align API's for table and collections views

---
+++ Sources/Protocols/DataViewable.swift
@@ -0,0 +1,19 @@
+//
+//  DataViewable.swift
+//  ZamzamKit iOS
+//
+//  Created by Basem Emara on 2018-02-04.
+//  Copyright © 2018 Zamzam. All rights reserved.
+//
+
+import UIKit
+
+public protocol DataViewable {
+    func reloadData()
+}
+
+// Align API's for table and collections views
+// http://basememara.com/protocol-oriented-tableview-collectionview/
+
+extension UITableView: DataViewable {}
+extension UICollectionView: DataViewable {}
