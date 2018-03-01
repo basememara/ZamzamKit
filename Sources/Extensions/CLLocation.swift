3f6b06a9e62e1d5575818fc54c32d6536edd95b7
Basem Emara <contact@basememara.com>
2018-03-01 13:47:48 -0500

    Add convenience extensions to CLLocationCoordinate2D

--- Sources/Extensions/CLLocation.swift
+++ Sources/Extensions/CLLocation.swift
@@ -35,18 +35,44 @@ public extension CLLocation {
 }
 
 public extension CLLocationCoordinate2D {
+    
+    /// Returns a location object.
+    var location: CLLocation {
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+    
+    /// Returns the distance (measured in meters) from the receiver’s location to the specified location.
+    func distance(from coordinate: CLLocationCoordinate2D) -> CLLocationDistance {
+        return location.distance(from: coordinate.location)
+    }
+}
+
+public extension CLLocationCoordinate2D {
     // About 100 meters accuracy
     // https://gis.stackexchange.com/a/8674
     private static let decimalPrecision = 3
     
-    /// Approximate comparison of coordinates rounded to 2 decimal places
+    /// Approximate comparison of coordinates rounded to 2 decimal places.
     static func ~~(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
         return lhs.latitude.rounded(toPlaces: CLLocationCoordinate2D.decimalPrecision) == rhs.latitude.rounded(toPlaces: CLLocationCoordinate2D.decimalPrecision)
             && lhs.longitude.rounded(toPlaces: CLLocationCoordinate2D.decimalPrecision) == rhs.longitude.rounded(toPlaces: CLLocationCoordinate2D.decimalPrecision)
     }
     
-    /// Approximate comparison of coordinates rounded to 2 decimal places
+    /// Approximate comparison of coordinates rounded to 2 decimal places.
     static func !~(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
         return !(lhs ~~ rhs)
+    }
+}
+
+public extension Array where Element == CLLocationCoordinate2D {
+    
+    /// Returns the closest coordinate to the specified location.
+    func closest(to coordinate: CLLocationCoordinate2D) -> CLLocationCoordinate2D? {
+        return self.min { $0.distance(from: coordinate) < $1.distance(from: coordinate) }
+    }
+    
+    /// Returns the farthest coordinate from the specified location.
+    func farthest(from coordinate: CLLocationCoordinate2D) -> CLLocationCoordinate2D? {
+        return self.max { $0.distance(from: coordinate) > $1.distance(from: coordinate) }
     }
 }
