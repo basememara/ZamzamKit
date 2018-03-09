37e9d6b5dd9afc9bdca8ab26e01f548d5f8c9cfc
Basem Emara <contact@basememara.com>
2018-03-09 17:19:22 -0500

    Expose background task helper

--- Sources/Models/BackgroundTask.swift
+++ Sources/Models/BackgroundTask.swift
@@ -10,29 +10,29 @@
 import UIKit
 
 /// Encapsulate iOS background tasks
-class BackgroundTask {
+open class BackgroundTask {
     private let application: UIApplication
     private var identifier = UIBackgroundTaskInvalid
     
-    init(application: UIApplication) {
+    public init(application: UIApplication) {
         self.application = application
     }
     
     /// Execute a process with an indefinite background task.
     /// The handler must call `end()` when it is done.
-    class func run(for application: UIApplication, handler: (BackgroundTask) -> ()) {
+    open class func run(for application: UIApplication, handler: (BackgroundTask) -> ()) {
         let backgroundTask = BackgroundTask(application: application)
         backgroundTask.begin()
         handler(backgroundTask)
     }
     
-    func begin() {
+    open func begin() {
         self.identifier = application.beginBackgroundTask {
             self.end()
         }
     }
     
-    func end() {
+    open func end() {
         if identifier != UIBackgroundTaskInvalid {
             application.endBackgroundTask(identifier)
         }
