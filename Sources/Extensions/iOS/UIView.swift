102303f85e2dc8d0964039a08e5cd055d167680e
Basem Emara <contact@basememara.com>
2018-06-24 07:16:59 -0400

    Add UIView border and radius extensions

--- Sources/Extensions/iOS/UIView.swift
+++ Sources/Extensions/iOS/UIView.swift
@@ -76,6 +76,42 @@ public extension UIView {
 }
 
 public extension UIView {
+    
+    /// Border color of view; also inspectable from Storyboard.
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        
+        set {
+            guard let color = newValue else {
+                layer.borderColor = nil
+                return
+            }
+            
+            layer.borderColor = color.cgColor
+        }
+    }
+    
+    /// Border width of view; also inspectable from Storyboard.
+    @IBInspectable var borderWidth: CGFloat {
+        get { return layer.borderWidth }
+        set { layer.borderWidth = newValue }
+    }
+    
+    /// Corner radius of view; also inspectable from Storyboard.
+    @IBInspectable var cornerRadius: CGFloat {
+        get { return layer.cornerRadius }
+        
+        set {
+            layer.masksToBounds = true
+            layer.cornerRadius = newValue
+        }
+    }
+}
+
+public extension UIView {
 	
 	/// Add shadow to view.
 	///
