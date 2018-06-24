37a8f6640859ce41cf4b72671d410d476d419878
Basem Emara <contact@basememara.com>
2018-06-24 08:04:52 -0400

    Minor renaming of model

--- Sources/Models/iOS/RoundedViews.swift
+++ Sources/Models/iOS/RoundedViews.swift
@@ -24,7 +24,7 @@ public class RoundedButton: UIButton {
     }
 }
 
-public class RoundedImage: UIImageView {
+public class RoundedImageView: UIImageView {
     
     public override func awakeFromNib() {
         super.awakeFromNib()
