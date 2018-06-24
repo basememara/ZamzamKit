9f7dba00b03d0864cebe23487ccc5196a75289d6
Basem Emara <contact@basememara.com>
2018-06-24 08:01:18 -0400

    Add rounded views subclasses

---
+++ Sources/Models/iOS/RoundedViews.swift
@@ -0,0 +1,39 @@
+//
+//  RoundedViews.swift
+//  ZamzamKit iOS
+//
+//  Created by Basem Emara on 2018-06-24.
+//  Copyright © 2018 Zamzam. All rights reserved.
+//
+
+import UIKit
+
+public class RoundedView: UIView {
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.height / 2
+    }
+}
+
+public class RoundedButton: UIButton {
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.height / 2
+    }
+}
+
+public class RoundedImage: UIImageView {
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.masksToBounds = true
+        
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.height / 2
+    }
+}
