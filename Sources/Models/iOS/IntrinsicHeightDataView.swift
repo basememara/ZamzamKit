b55c25a9c98c874bfdbda79b4bdd0919368d2f06
Basem Emara <contact@basememara.com>
2018-07-09 20:11:48 -0400

    Add dynamic table/collection view for auto-calculating intrinsic heights when nested in a stackview

---
+++ Sources/Models/iOS/IntrinsicHeightDataView.swift
@@ -0,0 +1,35 @@
+//
+//  IntrinsicHeightDataView.swift
+//  ZamzamKit iOS
+//
+//  Created by Basem Emara on 2018-07-09.
+//  Copyright © 2018 Zamzam. All rights reserved.
+//
+
+import UIKit
+
+/// CollectionView that auto-sizes in StackView
+public class IntrinsicHeightCollectionView: UICollectionView {
+    
+    public override var contentSize: CGSize {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    public override var intrinsicContentSize: CGSize {
+        layoutIfNeeded()
+        return CGSize(width: UIViewNoIntrinsicMetric, height: contentSize.height)
+    }
+}
+
+/// TableView that auto-sizes in StackView
+public class IntrinsicHeightTableView: UITableView {
+    
+    public override var contentSize: CGSize {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    public override var intrinsicContentSize: CGSize {
+        layoutIfNeeded()
+        return CGSize(width: UIViewNoIntrinsicMetric, height: contentSize.height)
+    }
+}
