16a47cbe014afb5bbe706cc010fbb075e7499fbf
Basem Emara <contact@basememara.com>
2018-09-06 01:19:14 -0400

    Add custom UIKit controls

---
+++ Sources/Models/iOS/GradientView.swift
@@ -0,0 +1,47 @@
+//
+//  GradientView.swift
+//  ZamzamKit iOS
+//  https://medium.com/@sakhabaevegor/create-a-color-gradient-on-the-storyboard-18ccfd8158c2
+//
+//  Created by Basem Emara on 2018-08-10.
+//  Copyright © 2018 Zamzam. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+open class GradientView: UIView {
+    
+    @IBInspectable open var firstColor: UIColor = .clear {
+        didSet { configure() }
+    }
+    
+    @IBInspectable open var secondColor: UIColor = .clear {
+        didSet { configure() }
+    }
+    
+    @IBInspectable open var isHorizontal: Bool = true {
+        didSet { configure() }
+    }
+    
+    override class open var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+}
+
+private extension GradientView {
+    
+    func configure() {
+        guard let layer = self.layer as? CAGradientLayer else { return }
+        
+        layer.colors = [firstColor, secondColor].map { $0.cgColor }
+        
+        layer.startPoint = isHorizontal
+            ? CGPoint(x: 0, y: 0.5)
+            : CGPoint(x: 0.5, y: 0)
+        
+        layer.endPoint = isHorizontal
+            ? CGPoint (x: 1, y: 0.5)
+            : CGPoint (x: 0.5, y: 1)
+    }
+}
---
+++ Sources/Models/iOS/PlaceholderTextView.swift
@@ -0,0 +1,85 @@
+//
+//  PlaceholderTextView.swift
+//  ZamzamKit iOS
+//
+//  Created by Basem Emara on 2018-07-11.
+//  Copyright © 2018 Zamzam. All rights reserved.
+//
+
+import UIKit
+
+open class PlaceholderTextView: UITextView {
+    
+    @IBInspectable open var placeholder: String = "" {
+        didSet { placeholderLabel.text = placeholder }
+    }
+    
+    @IBInspectable open var placeholderColor: UIColor = .lightGray {
+        didSet { placeholderLabel.textColor = placeholderColor }
+    }
+    
+    // Allow placeholders in UITextView
+    private lazy var placeholderLabel: UILabel = {
+        UILabel().with {
+            $0.text = placeholder
+            $0.numberOfLines = 0
+            $0.font = font
+            $0.textColor = placeholderColor
+        }
+    }()
+    
+    private let notificationCenter = NotificationCenter.default
+    private let padding = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
+    
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        textContainerInset = padding
+        
+        guard !placeholder.isEmpty else { return }
+        addSubview(placeholderLabel)
+        
+        // Set constraints
+        // https://useyourloaf.com/blog/pain-free-constraints-with-layout-anchors/
+        placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
+        placeholderLabel.topAnchor.constraint(equalTo: topAnchor, constant: padding.top).isActive = true
+        placeholderLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: padding.bottom).isActive = true
+        placeholderLabel.widthAnchor.constraint(
+            equalTo: widthAnchor,
+            constant: -(padding.left + padding.right)
+        ).isActive = true
+        placeholderLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        notificationCenter.addObserver(self, selector: #selector(didBeginEditing), name: .UITextViewTextDidBeginEditing, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(didChangeText), name: .UITextViewTextDidChange, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(didEndEditing), name: .UITextViewTextDidEndEditing, object: nil)
+    }
+    
+    open override var text: String! {
+        get { return super.text }
+        set {
+            super.text = newValue
+            
+            // Since observer does not fire when manually set
+            placeholderLabel.isHidden = !text.isEmpty
+        }
+    }
+}
+
+private extension PlaceholderTextView {
+    
+    @objc func didBeginEditing(_ notification: Notification) {
+        guard notification.object as? UITextView == self else { return }
+        placeholderLabel.isHidden = true
+    }
+    
+    @objc func didChangeText(_ notification: Notification) {
+        guard notification.object as? UITextView == self else { return }
+        placeholderLabel.isHidden = !text.isEmpty
+    }
+    
+    @objc func didEndEditing(_ notification: Notification) {
+        guard notification.object as? UITextView == self else { return }
+        placeholderLabel.isHidden = !text.isEmpty
+    }
+}
