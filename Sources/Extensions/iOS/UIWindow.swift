263de13c06f2b80f9fa07e5a4f23b182cd4ca1d9
Basem Emara <contact@basememara.com>
2018-09-27 08:26:20 -0400

    Add UIWindow extension for reloading views for needs such as applying UIAppearance changes

--- Sources/Extensions/iOS/UIWindow.swift
+++ Sources/Extensions/iOS/UIWindow.swift
@@ -10,6 +10,11 @@ import UIKit
 
 public extension UIWindow {
     
+    /// The top view controller for the window.
+    var topViewController: UIViewController? {
+        return getTopViewController(from: rootViewController)
+    }
+    
     /// Recursively retrieve the top most view controller
     /// https://stackoverflow.com/a/39857342/235334
     private func getTopViewController(from controller: UIViewController?) -> UIViewController? {
@@ -23,9 +28,25 @@ public extension UIWindow {
         
         return controller
     }
+}
+
+public extension UIWindow {
     
-    /// The top view controller for the window.
-    var topViewController: UIViewController? {
-        return getTopViewController(from: rootViewController)
+    /// Unload all views and add back.
+    /// Useful for applying `UIAppearance` changes to existing views.
+    func reload() {
+        subviews.forEach { view in
+            view.removeFromSuperview()
+            addSubview(view)
+        }
+    }
+}
+
+public extension Array where Element == UIWindow {
+    
+    /// Unload all views for each `UIWindow` and add back.
+    /// Useful for applying `UIAppearance` changes to existing views.
+    func reload() {
+        forEach { $0.reload() }
     }
 }
