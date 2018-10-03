cb558dc8d3b014bb2a3ab0ef580cff0ea2cd6aaa
Basem Emara <contact@basememara.com>
2018-10-03 18:30:29 -0400

    Add UI tests

--- ZamzamKit Example/Scenes/ControlsViewController.swift
+++ ZamzamKit Example/Scenes/ControlsViewController.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2018 Zamzam. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SystemConfiguration
+
+class ControlsViewController: UIViewController {
+    
+    @IBAction func alertButtonTapped() {
+        present(alert: "Test Alert")
+    }
+    
+    @IBAction func safariButtonTapped() {
+        present(safari: "https://apple.com")
+    }
+    
+    @IBAction func presentShareActivityTapped(_ sender: UIButton) {
+        guard let link = URL(string: "https://apple.com") else { return }
+        
+        let safariActivity = UIActivity.make(
+            title: .localized(.openInSafari),
+            imageName: "safari-share",
+            imageBundle: .zamzamKit,
+            handler: {
+                guard SCNetworkReachability.isOnline else {
+                    return self.present(alert: "Device must be online to view within the browser.")
+                }
+            
+                UIApplication.shared.open(link)
+            }
+        )
+        
+        present(
+            activities: ["Test Title", link],
+            popoverFrom: sender,
+            applicationActivities: [safariActivity]
+        )
+    }
+}
