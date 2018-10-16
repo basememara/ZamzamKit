e7ba6ca5ccb4081861670b9d3e84bb9107f27570
Basem Emara <contact@basememara.com>
2018-10-15 23:15:20 -0400

    Minor code refactor to table and collection cell register / dequeue

--- Sources/Extensions/iOS/UICollectionView.swift
+++ Sources/Extensions/iOS/UICollectionView.swift
@@ -10,25 +10,19 @@ import Foundation
 import UIKit
 
 public extension UICollectionView {
-
-    static var defaultCellIdentifier: String {
-        return "Cell"
-    }
+    static let defaultCellIdentifier = "Cell"
     
     /**
      Register NIB to collection view. NIB name and cell reuse identifier can match for convenience.
 
      - parameter nibName: Name of the NIB.
-     - parameter cellIdentifier: Name of the reusable cell identifier.
-     - parameter bundleIdentifier: Name of the bundle identifier if not local.
+     - parameter withIdentifier: Name of the reusable cell identifier.
+     - parameter inBundle: Bundle if not local.
      */
-    func register<T: UICollectionViewCell>(nib type: T.Type, cellIdentifier: String = defaultCellIdentifier, bundleIdentifier: String? = nil) {
+    func register<T: UICollectionViewCell>(nib type: T.Type, withReuseIdentifier identifier: String = UICollectionView.defaultCellIdentifier, inBundle bundle: Bundle? = nil) {
         register(
-            UINib(
-                nibName: String(describing: type),
-                bundle: bundleIdentifier != nil ? Bundle(identifier: bundleIdentifier!) : nil
-            ),
-            forCellWithReuseIdentifier: cellIdentifier
+            UINib(nibName: String(describing: type), bundle: bundle),
+            forCellWithReuseIdentifier: identifier
         )
     }
 }
@@ -61,11 +55,23 @@ public extension UICollectionView {
      Gets the reusable cell with the specified identifier name.
 
      - parameter indexPath: The index path of the cell from the collection.
-     - parameter identifier: Name of the reusable cell identifier.
+     - parameter withReuseIdentifier: Name of the reusable cell identifier.
 
      - returns: Returns the collection view cell.
      */
-    subscript(indexPath: IndexPath, identifier: String) -> UICollectionViewCell {
+    subscript(indexPath: IndexPath, withReuseIdentifier identifier: String) -> UICollectionViewCell {
         return dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+    }
+    
+    /**
+     Gets the reusable cell with default identifier name.
+     
+     - parameter indexPath: The index path of the cell from the table.
+     - parameter withReuseIdentifier: Name of the reusable cell identifier.
+     
+     - returns: Returns the table view cell.
+     */
+    subscript<T: UICollectionViewCell>(indexPath: IndexPath, withReuseIdentifier identifier: String) -> T {
+        return dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! T
     }
 }
--- Sources/Extensions/iOS/UITableView.swift
+++ Sources/Extensions/iOS/UITableView.swift
@@ -10,49 +10,37 @@ import Foundation
 import UIKit
 
 public extension UITableView {
-
-    static var defaultCellIdentifier: String {
-        return "Cell"
-    }
+    static let defaultCellIdentifier = "Cell"
     
     /**
      Register NIB to table view. NIB type and cell reuse identifier can match for convenience.
      
      - parameter nibName: Type of the NIB.
-     - parameter cellIdentifier: Name of the reusable cell identifier.
-     - parameter bundleIdentifier: Name of the bundle identifier if not local.
+     - parameter withIdentifier: Name of the reusable cell identifier.
+     - parameter inBundle: Bundle if not local.
      */
-    func register<T: UITableViewCell>(nib type: T.Type, cellIdentifier: String = defaultCellIdentifier, bundleIdentifier: String? = nil) {
+    func register<T: UITableViewCell>(nib type: T.Type, withIdentifier identifier: String = UITableView.defaultCellIdentifier, inBundle bundle: Bundle? = nil) {
         register(
-            UINib(
-                nibName: String(describing: type),
-                bundle: bundleIdentifier != nil ? Bundle(identifier: bundleIdentifier!) : nil
-            ),
-            forCellReuseIdentifier: cellIdentifier
+            UINib(nibName: String(describing: type), bundle: bundle),
+            forCellReuseIdentifier: identifier
         )
     }
 }
 
 public extension UITableView {
-    
-    static var defaultHeaderFooterIdentifier: String {
-        return "Header"
-    }
+    static let defaultHeaderFooterIdentifier = "Header"
     
     /**
      Register NIB to table view. NIB type and header/footer reuse identifier can match for convenience.
      
      - parameter nibName: Type of the NIB.
-     - parameter headerFooterIdentifier: Name of the reusable header/footer identifier.
-     - parameter bundleIdentifier: Name of the bundle identifier if not local.
+     - parameter withIdentifier: Name of the reusable cell identifier.
+     - parameter inBundle: Bundle if not local.
      */
-    func register<T: UITableViewHeaderFooterView>(nib type: T.Type, headerFooterIdentifier: String = defaultHeaderFooterIdentifier, bundleIdentifier: String? = nil) {
+    func register<T: UITableViewHeaderFooterView>(nib type: T.Type, withIdentifier identifier: String = UITableView.defaultHeaderFooterIdentifier, inBundle bundle: Bundle? = nil) {
         register(
-            UINib(
-                nibName: String(describing: type),
-                bundle: bundleIdentifier != nil ? Bundle(identifier: bundleIdentifier!) : nil
-            ),
-            forHeaderFooterViewReuseIdentifier: headerFooterIdentifier
+            UINib(nibName: String(describing: type), bundle: bundle),
+            forHeaderFooterViewReuseIdentifier: identifier
         )
     }
     
@@ -90,12 +78,24 @@ public extension UITableView {
      Gets the reusable cell with the specified identifier name.
 
      - parameter indexPath: The index path of the cell from the table.
-     - parameter identifier: Name of the reusable cell identifier.
+     - parameter withIdentifier: Name of the reusable cell identifier.
 
      - returns: Returns the table view cell.
      */
-    subscript(indexPath: IndexPath, identifier: String) -> UITableViewCell {
+    subscript(indexPath: IndexPath, withIdentifier identifier: String) -> UITableViewCell {
         return dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+    }
+    
+    /**
+     Gets the reusable cell with default identifier name.
+     
+     - parameter indexPath: The index path of the cell from the table.
+     - parameter withIdentifier: Name of the reusable cell identifier.
+     
+     - returns: Returns the table view cell.
+     */
+    subscript<T: UITableViewCell>(indexPath: IndexPath, withIdentifier identifier: String) -> T {
+        return dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! T
     }
 }
 
