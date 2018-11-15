f84980a11979974a313a8b945a82d5a808f66d99
Basem Emara <contact@basememara.com>
2018-11-14 23:49:03 -0500

    Add keyboard-friendly scroll view subclass

---
+++ Sources/Controls/iOS/KeyboardScrollView.swift
@@ -0,0 +1,57 @@
+//
+//  KeyboardScrollView.swift
+//  ZamzamKit iOS
+//
+//  Created by Basem Emara on 2018-11-14.
+//  Copyright © 2018 Zamzam. All rights reserved.
+//
+
+import UIKit
+
+public class KeyboardScrollView: UIScrollView {
+    private let notificationCenter: NotificationCenter = .default
+    
+    public var activeField: UIView?
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Dismiss keyboard when tapped out of textfield
+        endEditing(true)
+    }
+}
+
+public extension KeyboardScrollView {
+    
+    /// Adjusts layout to prevent keyboard overlaying UI elements
+    @IBInspectable var automaticallyAdjustsInsetsForKeyboard: Bool {
+        get { return false /* Not stored */ }
+        set {
+            guard newValue else { return }
+            notificationCenter.addObserver(for: UIResponder.keyboardWillShowNotification, selector: #selector(adjustsInsetsForKeyboard), from: self)
+            notificationCenter.addObserver(for: UIResponder.keyboardWillHideNotification, selector: #selector(adjustsInsetsForKeyboard), from: self)
+        }
+    }
+    
+    @objc private func adjustsInsetsForKeyboard(_ notification: NSNotification) {
+        guard let info = notification.userInfo,
+            let keyboardFrame = info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+                return
+        }
+        
+        let keyboardHeight = notification.name == UIResponder.keyboardWillShowNotification
+            ? keyboardFrame.cgRectValue.size.height : 0
+        
+        contentInset.bottom = keyboardHeight
+        scrollIndicatorInsets.bottom = keyboardHeight
+        
+        // Scroll to active text field if it is hidden by keyboard
+        // https://developer.apple.com/library/content/documentation/StringsTextFonts/Conceptual/TextAndWebiPhoneOS/KeyboardManagement/KeyboardManagement.html#//apple_ref/doc/uid/TP40009542-CH5-SW7
+        guard notification.name == UIResponder.keyboardWillShowNotification else { return }
+        scrollToActiveField()
+    }
+    
+    func scrollToActiveField() {
+        guard let activeField = activeField else { return }
+        let rect = activeField.convert(activeField.bounds, to: self)
+        scrollRectToVisible(rect, animated: true)
+    }
+}
