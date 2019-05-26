3ef56619fc523ce640433757485858ffc9e4bd85
Basem Emara <contact@basememara.com>
2019-05-26 09:00:13 -0400

    UIAppearance support for UIButton font

---
+++ Sources/Extensions/iOS/UIButton.swift
@@ -0,0 +1,19 @@
+//
+//  UIButton.swift
+//  ZamzamKit iOS
+//
+//  Created by Basem Emara on 2019-05-22.
+//  Copyright © 2019 Zamzam Inc. All rights reserved.
+//
+
+import UIKit
+
+public extension UIButton {
+    
+    /// The color of the title font with UIAppearance support.
+    @objc dynamic var titleFont: UIFont? {
+        // UIAppearance: https://stackoverflow.com/a/42410383
+        get { return titleLabel?.font }
+        set { titleLabel?.font = newValue }
+    }
+}
