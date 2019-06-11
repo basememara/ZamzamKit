2eda034c64109c484d066a1d1a54dc478f2f0c76
Basem Emara <contact@basememara.com>
2019-06-11 06:32:25 -0400

    Add Swift Package Manager manifest

---
+++ Package.swift
@@ -0,0 +1,69 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ZamzamKit",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "ZamzamCore",
+            targets: ["ZamzamCore"]
+        ),
+        .library(
+            name: "ZamzamLocation",
+            targets: ["ZamzamLocation"]
+        ),
+        .library(
+            name: "ZamzamNotification",
+            targets: ["ZamzamNotification"]
+        ),
+        .library(
+            name: "ZamzamUI",
+            targets: ["ZamzamUI"]
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "ZamzamCore",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "ZamzamCoreTests",
+            dependencies: ["ZamzamCore"]
+        ),
+        .target(
+            name: "ZamzamLocation",
+            dependencies: ["ZamzamCore"]
+        ),
+        .testTarget(
+            name: "ZamzamLocationTests",
+            dependencies: ["ZamzamLocation"]
+        ),
+        .target(
+            name: "ZamzamNotification",
+            dependencies: ["ZamzamCore"]
+        ),
+        .testTarget(
+            name: "ZamzamNotificationTests",
+            dependencies: ["ZamzamNotification"]
+        ),
+        .target(
+            name: "ZamzamUI",
+            dependencies: ["ZamzamCore"]
+        ),
+        .testTarget(
+            name: "ZamzamUITests",
+            dependencies: ["ZamzamUI"]
+        )
+    ]
+)
