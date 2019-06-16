ba7db9bc8a5887e4bf0e82e48e69352f7754d726
Basem Emara <contact@basememara.com>
2019-06-16 10:55:20 -0400

    Expose title to input accessory toolbar

--- Sources/ZamzamUI/iOS/Extensions/UIToolbar.swift
+++ Sources/ZamzamUI/iOS/Extensions/UIToolbar.swift
@@ -32,17 +32,15 @@ public extension UIToolbar {
     ///   - target: The object that receives the action message.
     ///   - action: The action to send to target when this item is selected.
     ///   - clearAction: The action to send to target when this item is cleared.
+    ///   - title: The title of the toolbar.
     /// - Returns: A control that displays the done button along the bottom edge of your interface.
-    static func makeInputDoneToolbar(target: Any?, action: Selector, clearAction: Selector? = nil) -> UIToolbar {
+    static func makeInputDoneToolbar(target: Any?, action: Selector, clearAction: Selector? = nil, title: String? = nil) -> UIToolbar {
         return UIToolbar().with {
             $0.isTranslucent = true
             $0.isUserInteractionEnabled = true
             $0.sizeToFit()
             
-            var items = [
-                UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
-                UIBarButtonItem(barButtonSystemItem: .done, target: target, action: action)
-            ]
+            var items = [UIBarButtonItem]()
             
             if let clearAction = clearAction {
                 let clearButton = UIBarButtonItem(
@@ -56,8 +54,28 @@ public extension UIToolbar {
                     .font: UIFont.boldSystemFont(ofSize: UIFont.labelFontSize)
                 ], for: .normal)
                 
-                items.insert(clearButton, at: 0)
+                items.append(clearButton)
             }
+            
+            if let title = title {
+                items += [
+                    .init(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+                    .init(customView:
+                        UILabel(frame: .zero).with {
+                            $0.text = title
+                            $0.sizeToFit()
+                            $0.backgroundColor = .clear
+                            $0.textColor = .gray
+                            $0.textAlignment = .center
+                        }
+                    )
+                ]
+            }
+            
+            items += [
+                .init(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+                .init(barButtonSystemItem: .done, target: target, action: action)
+            ]
             
             $0.setItems(items, animated: false)
         }
@@ -69,12 +87,45 @@ public extension UIToolbar {
     ///   - target: The object that receives the action message.
     ///   - action: The action to send to target when this item is selected.
     ///   - clearAction: The action to send to target when this item is cleared.
+    ///   - title: The title of the toolbar.
     /// - Returns: A control that displays the next button along the bottom edge of your interface.
-    static func makeInputNextToolbar(target: Any?, action: Selector, clearAction: Selector? = nil) -> UIToolbar {
+    static func makeInputNextToolbar(target: Any?, action: Selector, clearAction: Selector? = nil, title: String? = nil) -> UIToolbar {
         return UIToolbar().with {
             $0.isTranslucent = true
             $0.isUserInteractionEnabled = true
             $0.sizeToFit()
+            
+            var items = [UIBarButtonItem]()
+            
+            if let clearAction = clearAction {
+                let clearButton = UIBarButtonItem(
+                    title: .localized(.clear),
+                    style: .plain,
+                    target: target,
+                    action: clearAction
+                )
+                
+                clearButton.setTitleTextAttributes([
+                    .font: UIFont.boldSystemFont(ofSize: UIFont.labelFontSize)
+                ], for: .normal)
+                
+                items.append(clearButton)
+            }
+            
+            if let title = title {
+                items += [
+                    .init(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+                    .init(customView:
+                        UILabel(frame: .zero).with {
+                            $0.text = title
+                            $0.sizeToFit()
+                            $0.backgroundColor = .clear
+                            $0.textColor = .gray
+                            $0.textAlignment = .center
+                        }
+                    )
+                ]
+            }
             
             let nextButton = UIBarButtonItem(
                 title: .localized(.next),
@@ -87,25 +138,10 @@ public extension UIToolbar {
                 .font: UIFont.boldSystemFont(ofSize: UIFont.labelFontSize)
             ], for: .normal)
             
-            var items = [
-                UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            items += [
+                .init(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
                 nextButton
             ]
-            
-            if let clearAction = clearAction {
-                let clearButton = UIBarButtonItem(
-                    title: .localized(.clear),
-                    style: .plain,
-                    target: target,
-                    action: clearAction
-                )
-                
-                clearButton.setTitleTextAttributes([
-                    .font: UIFont.boldSystemFont(ofSize: UIFont.labelFontSize)
-                ], for: .normal)
-                
-                items.insert(clearButton, at: 0)
-            }
             
             $0.setItems(items, animated: false)
         }
