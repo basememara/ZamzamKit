c3bab62028bad11ed0ed0d263cf9e4ff10c5c00b
Basem Emara <contact@basememara.com>
2019-07-10 18:18:50 -0400

    Extend Audible protocol to support data assets

--- Sources/ZamzamUI/iOS/Controls/Protocols/Audible.swift
+++ Sources/ZamzamUI/iOS/Controls/Protocols/Audible.swift
@@ -15,7 +15,7 @@ public protocol Audible: class {
 
 public extension Audible {
     
-    func setupAudioPlayer(_ application: UIApplication, fileName: String, bundle: Bundle = .main) {
+    func setupAudioPlayer(_ application: UIApplication, forResource fileName: String, bundle: Bundle = .main) {
         guard let sound = bundle.url(forResource: fileName, withExtension: nil),
             (audioPlayer == nil || audioPlayer?.url != sound) else {
                 return
@@ -32,6 +32,27 @@ public extension Audible {
         } catch {
             
         }
+    }
+}
+
+public extension Audible {
+    
+    func setupAudioPlayer(_ application: UIApplication, forAsset assetName: String, type: String? = nil, bundle: Bundle = .main) {
+        guard let sound = NSDataAsset(name: assetName, bundle: bundle)?.data,
+            (audioPlayer == nil || audioPlayer?.data != sound) else {
+                return
+        }
         
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            application.beginReceivingRemoteControlEvents()
+            
+            audioPlayer = try AVAudioPlayer(data: sound, fileTypeHint: type)
+            audioPlayer?.prepareToPlay()
+        } catch {
+            
+        }
     }
 }
