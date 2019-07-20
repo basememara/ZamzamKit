6c3dc61b19f16fc1c940cf326a3ba64284ef4f53
Basem Emara <contact@basememara.com>
2019-07-19 23:35:36 -0400

    Split up routable by platform

--- Sources/ZamzamUI/Scene/AppRoutable.swift
+++ Sources/ZamzamUI/Scene/AppRoutable.swift
@@ -6,45 +6,20 @@
 //  Copyright © 2018 Zamzam Inc. All rights reserved.
 //
 
+#if os(iOS)
 import UIKit
+#elseif os(watchOS)
+import WatchKit
+#endif
 
 public protocol AppRoutable {
+    #if os(iOS)
     var viewController: UIViewController? { get set }
+    #elseif os(watchOS)
+    var viewController: WKInterfaceController? { get set }
+    #endif
     
     func dismiss(animated: Bool, completion: (() -> Void)?)
     func dismissOrPop(animated: Bool, completion: (() -> Void)?)
     func close(animated: Bool, completion: (() -> Void)?)
-}
-
-public extension AppRoutable {
-    
-    /// Dismisses the view controller that was presented.
-    ///
-    /// - Parameters:
-    ///   - completion: The block to execute after the view controller is dismissed.
-    func dismiss(animated: Bool = true, completion: (() -> Void)? = nil) {
-        viewController?.dismiss(animated: animated, completion: completion)
-    }
-    
-    /// Dismisses or pops the view controller that was presented modally by the view controller.
-    ///
-    /// This method dismisses the view controller or pops it if it is part of a navigation controller.
-    /// If the view controller is the last in the stack of a navigation controller, this method will
-    /// dismiss the navigation controller. In other words, it will no longer be presented modally.
-    ///
-    /// - Parameters:
-    ///   - animated: Set this value to true to animate the transition.
-    ///   - completion: The block to execute after the the task finishes.
-    func dismissOrPop(animated: Bool = true, completion: (() -> Void)? = nil) {
-        viewController?.dismissOrPop(animated: animated, completion: completion)
-    }
-    
-    /// Closes the view controller and container that was presenting it.
-    ///
-    /// - Parameters:
-    ///   - animated: Set this value to true to animate the transition.
-    ///   - completion: The block to execute after the the task finishes.
-    func close(animated: Bool = true, completion: (() -> Void)? = nil) {
-        viewController?.close(animated: animated, completion: completion)
-    }
 }
