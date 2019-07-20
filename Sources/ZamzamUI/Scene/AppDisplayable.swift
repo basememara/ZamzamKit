fd5820396d40625d421f042bfea801993853ad28
Basem Emara <contact@basememara.com>
2019-07-19 23:26:54 -0400

    Split up displayable scenes into targets

--- Sources/ZamzamUI/Scene/AppDisplayable.swift
+++ Sources/ZamzamUI/Scene/AppDisplayable.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2018 Zamzam Inc. All rights reserved.
 //
 
-import UIKit
-
 /// Super displayer for implementing global extensions.
 public protocol AppDisplayable {
     
@@ -19,33 +17,3 @@ public protocol AppDisplayable {
     /// Hides spinners, loaders, and anything else
     func endRefreshing()
 }
-
-#if os(iOS)
-extension AppDisplayable where Self: UIViewController {
-    
-    /// Display a native alert controller modally.
-    ///
-    /// - Parameter error: The error details to present.
-    public func display(error: AppModels.Error) {
-        // Force in next runloop via main queue since view hierachy may not be loaded yet
-        DispatchQueue.main.async { [weak self] in
-            self?.endRefreshing()
-            self?.present(alert: error.title, message: error.message)
-        }
-    }
-}
-#elseif os(iOS)
-extension AppDisplayable where Self: WKInterfaceController {
-    
-    /// Display a native alert controller modally.
-    ///
-    /// - Parameter error: The error details to present.
-    public func display(error: AppModels.Error) {
-        // Force in next runloop via main queue since view hierachy may not be loaded yet
-        DispatchQueue.main.async { [weak self] in
-            self?.endRefreshing()
-            self?.present(alert: error.title, message: error.message)
-        }
-    }
-}
-#endif
