9327999d939552d419201ca46a363321ba6c764a
Basem Emara <contact@basememara.com>
2019-08-23 01:31:34 -0400

    Update SPM dependencies

--- Package.swift
+++ Package.swift
@@ -38,9 +38,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ZamzamKit",
-            dependencies: [
-                .product(name: "ZamzamCore")
-            ]
+            dependencies: ["ZamzamCore"]
         ),
         .testTarget(
             name: "ZamzamKitTests",
