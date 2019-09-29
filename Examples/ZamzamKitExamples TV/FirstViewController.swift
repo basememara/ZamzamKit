e445ec6bd6cca7a2564b738043aa70f96f66fae4
Basem Emara <contact@basememara.com>
2019-09-29 08:54:20 -0400

    Rename dependency injection file

---
+++ Examples/ZamzamKitExamples TV/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ZamzamKitExamples TV
+//
+//  Created by Basem Emara on 2019-09-28.
+//  Copyright © 2019 Zamzam Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Examples/ZamzamKitExamples iOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ZamzamKitExamples iOS
+//
+//  Created by Basem Emara on 2019-09-28.
+//  Copyright © 2019 Zamzam Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
