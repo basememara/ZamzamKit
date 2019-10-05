7a33cc79257bffee2ab75d481fba09b584092422
Basem Emara <contact@basememara.com>
2019-10-05 10:46:49 -0400

    Use concurrent queue w/ barrier for thread-safety

--- Sources/ZamzamCore/Utilities/Synchronized.swift
+++ Sources/ZamzamCore/Utilities/Synchronized.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  Synchronized.swift
+//  https://basememara.com/creating-thread-safe-generic-values-in-swift/
 //
 //  Created by Basem Emara on 2019-10-03.
 //
@@ -9,18 +9,18 @@ import Foundation
 
 /// An object that manages the execution of tasks atomically.
 public struct Synchronized<Value> {
-    private let mutex = DispatchSemaphore(value: 1)
+    private let mutex = DispatchQueue(label: "io.zamzam.Synchronized", attributes: .concurrent)
     private var _value: Value
 
     public init(_ value: Value) {
         self._value = value
     }
-    
-    /// Returns or modify the value.
-    public var value: Value { mutex.lock { _value } }
-    
-    /// Submits a block for synchronous execution with this lock.
+
+    /// Returns or modify the thread-safe value.
+    public var value: Value { mutex.sync { _value } }
+
+    /// Submits a block for synchronous, thread-safe execution.
     public mutating func value<T>(execute task: (inout Value) throws -> T) rethrows -> T {
-        try mutex.lock { try task(&_value) }
+        try mutex.sync(flags: .barrier) { try task(&_value) }
     }
 }
--- Tests/ZamzamCoreTests/SynchronizedTests.swift
+++ Tests/ZamzamCoreTests/SynchronizedTests.swift
@@ -9,22 +9,41 @@ import XCTest
 import ZamzamCore
 
 final class SynchronizedTests: XCTestCase {
-    
+    private let iterations = 1_000_000
+    private let writeMultipleOf = 1000
 }
 
 extension SynchronizedTests {
     
-    func testLockPerformance() {
+    func testWritePerformance() {
         var temp = Synchronized<Int>(0)
-
+     
         measure {
             temp.value { $0 = 0 } // Reset
-            
-            DispatchQueue.concurrentPerform(iterations: 1_000_000) { _ in
+     
+            DispatchQueue.concurrentPerform(iterations: iterations) { _ in
                 temp.value { $0 += 1 }
             }
             
-            XCTAssertEqual(temp.value, 1_000_000)
+            XCTAssertEqual(temp.value, iterations)
+        }
+    }
+    
+    func testReadPerformance() {
+        var temp = Synchronized<Int>(0)
+     
+        measure {
+            temp.value { $0 = 0 } // Reset
+     
+            DispatchQueue.concurrentPerform(iterations: iterations) {
+                XCTAssertGreaterThanOrEqual(temp.value, 0)
+                
+                if $0.isMultiple(of: writeMultipleOf) {
+                    temp.value { $0 += 1 }
+                }
+            }
+            
+            XCTAssertGreaterThanOrEqual(temp.value, 0)
         }
     }
 }
