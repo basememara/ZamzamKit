6dafd2a6e6484467fccbe925c4304f6f6a721951
Basem Emara <contact@basememara.com>
2019-10-12 21:27:22 -0400

    Apply comments to preferences protocol

--- Sources/ZamzamCore/Preferences/PreferencesAPI.swift
+++ Sources/ZamzamCore/Preferences/PreferencesAPI.swift
@@ -7,8 +7,23 @@
 //
 
 public protocol PreferencesStore {
+    
+    /// Retrieves the value from user defaults that corresponds to the given key.
+    ///
+    /// - Parameter key: The key that is used to read the user defaults item.
     func get<T>(_ key: String.Key<T?>) -> T?
+    
+    /// Stores the value in the user defaults item under the given key.
+    ///
+    /// - Parameters:
+    ///   - value: Value to be written to the user defaults.
+    ///   - key: Key under which the value is stored in the user defaults.
     func set<T>(_ value: T?, forKey key: String.Key<T?>)
+    
+    /// Deletes the single user defaults item specified by the key.
+    ///
+    /// - Parameter key: The key that is used to delete the user default item.
+    /// - Returns: True if the item was successfully deleted.
     func remove<T>(_ key: String.Key<T?>)
 }
 
