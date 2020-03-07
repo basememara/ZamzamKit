3108cbbc0963a8f22eea1cbabdc1d5fc9dab1e92
Basem Emara <contact@basememara.com>
2020-03-07 12:56:40 -0500

    Fix network comments

--- Sources/ZamzamCore/Network/NetworkAPI.swift
+++ Sources/ZamzamCore/Network/NetworkAPI.swift
@@ -15,40 +15,40 @@ public protocol NetworkStore {
     func send(with request: URLRequest, completion: @escaping (Result<NetworkAPI.Response, NetworkError>) -> Void)
 }
 
-/// The wrapper to handle HTTP requests.
+/// A thin wrapper to handle HTTP requests.
+///
+///     let request = URLRequest(
+///         url: URL(string: "https://httpbin.org/get")!,
+///         method: .get,
+///         parameters: [
+///             "abc": 123,
+///             "def": "test456",
+///             "xyz": true
+///         ],
+///         headers: [
+///             "Abc": "test123",
+///             "Def": "test456",
+///             "Xyz": "test789"
+///         ]
+///     )
+///
+///     let networkProvider = NetworkProvider(
+///         store: NetworkURLSessionStore()
+///     )
+///
+///     networkProvider.send(with: request) { result in
+///         switch result {
+///         case .success(let response):
+///             response.data
+///             response.headers
+///             response.statusCode
+///         case .failure(let error):
+///             error.statusCode
+///         }
+///     }
 public protocol NetworkProviderType {
     
     /// Creates a task that retrieves the contents of a URL based on the specified request object, and calls a handler upon completion.
-    ///
-    ///         let request = URLRequest(
-    ///             url: URL(string: "https://httpbin.org/get")!,
-    ///             method: .get,
-    ///             parameters: [
-    ///                 "abc": 123,
-    ///                 "def": "test456",
-    ///                 "xyz": true
-    ///             ],
-    ///             headers: [
-    ///                 "Abc": "test123",
-    ///                 "Def": "test456",
-    ///                 "Xyz": "test789"
-    ///             ]
-    ///         )
-    ///
-    ///         let networkProvider = NetworkProvider(
-    ///             store: NetworkURLSessionStore()
-    ///         )
-    ///
-    ///         networkProvider.send(with: request) { result in
-    ///             switch result {
-    ///             case .success(let response):
-    ///                 response.data
-    ///                 response.headers
-    ///                 response.statusCode
-    ///             case .failure(let error):
-    ///                 error.statusCode
-    ///             }
-    ///         }
     ///
     /// - Parameters:
     ///   - request: A network request object that provides the URL, parameters, headers, and so on.
