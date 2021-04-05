c4869a598021a15b10b92e03361a0a32f7a7e4fb
Basem Emara <basem@zamzam.io>
2021-04-05 13:10:56 -0400

    Add corner radius extension

--- Sources/ZamzamUI/Extensions/View.swift
+++ Sources/ZamzamUI/Extensions/View.swift
@@ -14,3 +14,24 @@ public extension View {
         AnyView(self)
     }
 }
+
+public extension View {
+    /// Clips this view to its bounding frame, with the specified corner radius.
+    ///
+    ///     Text("Rounded Corners")
+    ///         .frame(width: 175, height: 75)
+    ///         .foregroundColor(Color.white)
+    ///         .background(Color.black)
+    ///         .cornerRadius(25, corners: [.topLeft, .topRight])
+    ///
+    /// Creates and returns a new Bézier path object with a rectangular path rounded at the specified corners.
+    /// For rounding all corners, use the native `.cornerRadius(_ radius:)` function.
+    ///
+    /// - Parameters:
+    ///   - radius: The corner radius.
+    ///   - corners: A bitmask value that identifies the corners that you want rounded.
+    ///     You can use this parameter to round only a subset of the corners of the rectangle.
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedRect(radius: radius, corners: corners))
+    }
+}
