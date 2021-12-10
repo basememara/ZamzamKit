666403f527a9c4799706e3d534b419185fefd52a
Basem Emara <basem@zamzam.io>
2021-12-10 17:50:23 -0500

    Remove unneeded decodable

--- Sources/ZamzamCore/Extensions/CaseIterable.swift
+++ Sources/ZamzamCore/Extensions/CaseIterable.swift
@@ -22,37 +22,3 @@ public extension CaseIterable where Self: Equatable, AllCases: BidirectionalColl
         return array[safe: array.index(after: currentIndex)]
     }
 }
-
-// MARK: Decoding
-
-/// A protocol that uses the last case iterable case there is no matching decodable value.
-///
-/// The `status` value in the JSON string below is not a valid `Status` case. Instead of allowing the decoding
-/// process to fail, this protocol has an initializer extension that will fallback to the last case of the enum as default.
-///
-///     struct Example: Decodable {
-///         let status: Status
-///     }
-///
-///     enum Status: String, CaseIterableDefaultsLastDecodable {
-///         case abc
-///         case xyz
-///         case unknown
-///     }
-///
-///     let json = [{"status": "abc"}, {"status": "zzz"}].data(using: .utf8)
-///     let object = try JSONDecoder().decode([Example].self, from: json)
-///     print(object[0].status) // .abc
-///     print(object[1].status) // .unknown
-public protocol CaseIterableDefaultsLastDecodable: Decodable, CaseIterable, RawRepresentable
-    where RawValue: Decodable, AllCases: BidirectionalCollection {}
-
-public extension CaseIterableDefaultsLastDecodable {
-    init(from decoder: Decoder) throws {
-        // https://stackoverflow.com/a/49697266
-        self = try Self(
-            rawValue: decoder.singleValueContainer().decode()
-        ) ?? Self.allCases.last!
-        // swiftlint:disable:previous force_unwrapping
-    }
-}
--- Tests/ZamzamCoreTests/Extensions/CaseIterableTests.swift
+++ Tests/ZamzamCoreTests/Extensions/CaseIterableTests.swift
@@ -31,37 +31,3 @@ extension CaseIterableTests {
         XCTAssertEqual(Direction.south.next(), .west)
     }
 }
-
-extension CaseIterableTests {
-    func testCaseIterableDefaultsLastDecode() throws {
-        // Given
-        struct Example: Decodable {
-            let status: Status
-        }
-
-        enum Status: String, CaseIterableDefaultsLastDecodable {
-            case one
-            case two
-            case three
-            case unknown
-        }
-
-        let json = try XCTUnwrap(
-            """
-             [
-                 { "status": "one" },
-                 { "status": "two" },
-                 { "status": "something" }
-             ]
-             """.data(using: .utf8)
-        )
-
-        // When
-        let object = try JSONDecoder().decode([Example].self, from: json)
-
-        // Then
-        XCTAssertEqual(object[0].status, .one)
-        XCTAssertEqual(object[1].status, .two)
-        XCTAssertEqual(object[2].status, .unknown)
-    }
-}
