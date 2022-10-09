6a261953e6053de2be29cb264fc4902b40f60f46
Basem Emara <basem@zamzam.io>
2022-10-09 11:59:02 -0400

    Add Concurrency Combine sink convenience API

--- Sources/ZamzamCore/Extensions/Publisher.swift
+++ Sources/ZamzamCore/Extensions/Publisher.swift
@@ -8,6 +8,13 @@
 
 import Combine
 
+public extension Publisher where Failure == Never {
+    /// Attaches a subscriber with closure-based behavior to a publisher that never fails.
+    func sink(receiveValue: @escaping ((Self.Output) async throws -> Void)) -> AnyCancellable {
+        sink { value in Task { try await receiveValue(value) } }
+    }
+}
+
 public extension Optional where Wrapped: Combine.Publisher {
     /// Returns the wrapped value of a publisher or defaults to an empty publisher.
     func orEmpty() -> AnyPublisher<Wrapped.Output, Wrapped.Failure> {
